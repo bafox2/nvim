ceaeedf712764cba6bbd43c0aa8ee0bf68efa820
ben <ben@bfox.dev>
2024-02-26 20:50:01 -0500

    telescope keybinds

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,33 +1,26 @@
-local keymap = vim.keymap
-
-local config = function()
-    local telescope = require('telescope')
-    telescope.setup({
-        defaults = {
-            mappings = {
-                -- Your mappings here
-            },
-        },
-        pickers = {
-            find_files = {
-                -- Configuration for find_files picker
-            },
-            live_grep = {
-                -- Configuration for live_grep picker
-            },
-            find_buffers = {
-                -- Configuration for find_buffers picker
-            },
-        },
-    })
-end
-
 return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' },
     lazy = false,
-    config = config,
-    keys = {},
+    config = function()
+        require('telescope').setup({})
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>ff', function()
+            builtin.find_files()
+        end)
+        vim.keymap.set('n', '<leader>fg', function()
+            builtin.live_grep()
+        end)
+        vim.keymap.set('n', '<leader>fcw', function()
+            local word = vim.fn.expand("<cWORD>")
+            builtin.grep_string({ search = word })
+        end)
+        vim.keymap.set('n', '<leader>fh', function()
+            builtin.help_tags()
+        end)
+        vim.keymap.set('n', '<leader>fs', function()
+            builtin.grep_string({ search = vim.fn.input("Grep > ") })
+        end)
+    end
 }
-
